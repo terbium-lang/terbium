b54d892fad4f74979fce26a5e07f85a235dd1552
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-05-07 16:49:42 -0700

    InvalidToken struct

--- terbium_grammar/src/error.rs
+++ terbium_grammar/src/error.rs
@@ -1,1 +1,12 @@
+use std::fmt::{Display, Result as FmtResult};
 
+pub struct InvalidToken {
+    pub start: usize,
+    pub end: usize,
+}
+
+impl Display for InvalidToken {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> FmtResult {
+        write!(f, "Invalid token: from pos {} to pos {}", self.start, self.end)
+    }
+}
--- terbium_grammar/src/lib.rs
+++ terbium_grammar/src/lib.rs
@@ -3,3 +3,4 @@ pub mod error;
 pub mod token;
 
 pub use crate::token::Token;
+pub use crate::error::*;
