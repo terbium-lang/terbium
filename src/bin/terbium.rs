373b3b1dcc9e55643dfdbaf628410721714cc39a
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-05-10 19:21:06 -0400

    Implement CLI tool for AST debugging

--- src/bin/terbium.rs
+++ src/bin/terbium.rs
@@ -1,15 +1,63 @@
-use clap::Parser;
-use terbium::run;
+use terbium::{AstError, AstNode, AstParseInterface};
 
-/// Terbium CLI.
+use clap::{Parser, Subcommand};
+
 #[derive(Debug, Parser)]
-#[clap(author, version, about, long_about = None)]
-struct Args {}
+#[clap(name = "terbium")]
+#[clap(version, about, long_about = None)]
+/// The Terbium command-line interface.
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
 
-fn main() {
-    let _ = Args::parse(); // TODO: Handle CLI arguments.
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Transforms the Terbium source code into a formatted abstract syntax tree.
+    #[clap(arg_required_else_help = true)]
+    Ast {
+        /// The input file containing Terbium source code.
+        file: Option<String>,
 
-    println!("你好曹先生");
+        /// The direct source code to parse. Cannot be used with the file argument.
+        #[clap(short, long)]
+        code: Option<String>,
 
-    run(); // TODO: Pass in options from CLI;
+        /// Whether to output the AST in a "prettier" and more readable format.
+        #[clap(short, long)]
+        pretty: bool,
+    },
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Ast { file, code, pretty } => {
+            let ast = match (file, code) {
+                (Some(file), None) => std::fs::read_to_string(file)?,
+                (None, Some(code)) => code,
+                (Some(_), Some(_)) => Err("must provide only one of file or code")?,
+                (None, None) => Err("must provide one of file or code")?,
+            };
+
+            let (node, errors) = match AstNode::from_string(ast) {
+                Ok(t) => t,
+                Err(e) => {
+                    e.iter().for_each(AstError::print);
+                    return Err("syntax does not match grammar".into());
+                },
+            };
+            if pretty {
+                println!("{:#?}", node);
+                println!("Errors: {:#?}", errors);
+            }
+            else {
+                println!("{:?}", node);
+                println!("Errors: {:?}", errors);
+            }
+        }
+    }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
+pub use terbium_grammar::{
+    Token as AstToken,
+    Operator as AstOperator,
+    Body as AstBody,
+    Expr as AstExpr,
+    Node as AstNode,
+    Error as AstError,
+    ParseInterface as AstParseInterface,
+};
+
 pub fn run() {}
