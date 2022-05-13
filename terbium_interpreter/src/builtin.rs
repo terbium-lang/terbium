1e0421f63608567f1c0baf43da06ae72f5cbdc3d
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-05-12 20:10:24 -0400

    terbium_interpreter: Halt progress before interpreter rework

--- terbium_interpreter/src/builtin.rs
+++ terbium_interpreter/src/builtin.rs
@@ -1,22 +1,30 @@
 use super::{Interpreter, NativeFunctionResult, TerbiumExceptionType, TerbiumType, WrappedTerbiumObject};
 
-pub(crate) fn int_op_add(
-    interpreter: &mut Interpreter,
-    args: Vec<WrappedTerbiumObject>,
-) -> NativeFunctionResult {
-    if args.len() != 2 {
-        Err(TerbiumExceptionType::SignatureError("add op takes exactly 2 arguments"))?;
-    }
+macro_rules! bin_op {
+    ($op:literal, $name:ident, $t:expr, $subject:pat => $body:block) => {
+        pub(crate) fn $name(interpreter: &mut Interpreter, args: Vec<WrappedTerbiumObject>) -> NativeFunctionResult {
+            if args.len() != 2 {
+                Err(TerbiumExceptionType::SignatureError(
+                    format!("{} op takes exactly 2 arguments", $op),
+                ))?;
+            }
 
-    let int_type = interpreter.clone().singletons.int_type;
-    if interpreter.is_instance(&args[0], &int_type)
-        && interpreter.is_instance(&args[1], &int_type) 
-    {
-        return Ok(match (args[0].read().unwrap().clone().ty, args[1].read().unwrap().clone().ty) {
-            (TerbiumType::Int(a), TerbiumType::Int(b)) => interpreter.get_int(a + b),
-            _ => unreachable!(),
-        });
-    }
+            if interpreter.is_instance(&args[0], $t) && interpreter.is_instance(&args[1], $t) {
+                return Ok(match (args[0].read().unwrap().clone().ty, args[1].read.unwrap().clone().ty) {
+                    $subject => $body,
+                });
+            }
 
-    Err(TerbiumExceptionType::TypeError("expected int"))
+            Err(TerbiumExceptionType::TypeError(
+                format!("expected {}", $t.name),
+            ))
+        }
+    }
 }
+
+bin_op!(
+    "add",
+    int_op_add,
+    interpreter.singletons.int_type,
+    (TerbiumType::Int(a), TerbiumType::Int(b)) => interpreter.get_int(a + b)
+);
