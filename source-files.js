bc6f059d0a1e4fd412285fd2bd1bb35c4b86ba5a
jay3332 <jay3332@users.noreply.github.com>
2022-06-29 15:44:06 +0000

    deploy: 024d5c0434f50ba026adfa539963e9f3dbd99cbf

--- source-files.js
+++ source-files.js
@@ -2,6 +2,7 @@ var sourcesIndex = {};
 sourcesIndex["terbium"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["terbium_analyzer"] = {"name":"","files":["lib.rs","util.rs"]};
 sourcesIndex["terbium_bytecode"] = {"name":"","files":["interpreter.rs","lib.rs","util.rs"]};
+sourcesIndex["terbium_compiler"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["terbium_grammar"] = {"name":"","files":["ast.rs","error.rs","lib.rs","token.rs"]};
 sourcesIndex["terbium_interpreter"] = {"name":"","files":["interner.rs","lib.rs"]};
 createSourceSidebar();
