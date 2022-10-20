6cdd09603f340b4fe9bd6018250d79f861041f59
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-10-20 16:49:54 -0400

    implement range models

---
+++ grammar/src/lib.rs
@@ -0,0 +1,61 @@
+/// Represents the span of a token or a node in the AST. Can be represented as [start, end).
+#[derive(Copy, Clone, Debug)]
+pub struct Span {
+    /// The index of the first byte of the span.
+    pub start: usize,
+    /// One more than the index of the last byte of the span.
+    pub end: usize,
+}
+
+mod sealed {
+    use std::ops::{Range, RangeInclusive};
+
+    pub trait RangeInclusiveExt {
+        fn to_range(self) -> RangeInclusive<usize>;
+    }
+
+    impl RangeInclusiveExt for RangeInclusive<usize> {
+        fn to_range(self) -> RangeInclusive<usize> {
+            self
+        }
+    }
+
+    impl RangeInclusiveExt for Range<usize> {
+        fn to_range(self) -> RangeInclusive<usize> {
+            self.start..=self.end
+        }
+    }
+}
+
+impl Span {
+    /// Creates a new span from the given start, end, and source.
+    #[must_use]
+    pub(crate) fn new(start: usize, end: usize) -> Self {
+        Self { start, end }
+    }
+
+    /// Creates a new span from the given range and source.
+    #[must_use]
+    pub fn from_range<R: sealed::RangeInclusiveExt>(range: R) -> Self {
+        let range = range.to_range();
+        Self::new(*range.start(), *range.end())
+    }
+
+    /// Creates a single-length span.
+    #[must_use]
+    pub fn single(index: usize) -> Self {
+        Self::new(index, index + 1)
+    }
+
+    /// Returns the length of the span.
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.end - self.start
+    }
+}
+
+impl<R: sealed::RangeInclusiveExt> From<R> for Span {
+    fn from(range: R) -> Self {
+        Self::from_range(range)
+    }
+}
